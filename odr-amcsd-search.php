2d52113c4650e3a16c9ce2a3d0ca555500d6b955
Nate Stone <nate.stone@opendatarepository.org>
2026-02-13 07:20:57 +0000

    CSS Fix

--- odr-amcsd-search.php
+++ odr-amcsd-search.php
@@ -35,7 +35,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-const ODR_AMCSD_SEARCH_VERSION = '1.3.0';
+const ODR_AMCSD_SEARCH_VERSION = '1.3.1';
 
 /**
  * The code that runs during plugin activation.
